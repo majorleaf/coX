18cc81f35fd9b73f442c223f69b16ca1878075c3
Madandola james <jamesmadandola@gmail.com>
2026-09-10 07:59:03 +0100

    plain http, socket.io

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,34 @@
 import express from 'express';
-import executeRouter from './routes/execute';
+import http from 'http';
+import { Server as SocketIOServer } from 'socket.io';
+import executeRouter, { setSocketServer } from './routes/execute';
 
 const app = express();
 app.use(express.json());
 app.use('/', executeRouter);
+
+// wraps the express app in a created plain http to enable
+//socket.io attachment alongside normal HTTP routes
+const httpServer = http.createServer(app);
+
+const io = new SocketIOServer(httpServer, {
+  cors: { origin: '*'}// for production later
+});
+
+io.on('connection', (socket) => {
+  socket.on('subscribe', (jobId: string) => {
+    socket.join(jobId);
+    console.log(`Socket ${socket.id} subscribed to job ${jobId}`);
+  });
+  socket.on('disconnect', () => {
+    console.log(`client disconnected: ${socket.id}`);
+  });
+});
+
+// Give the routes module a reference to `io` so it can emit events
+// from inside runInSandbox's progress callback.
+setSocketServer(io);
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
